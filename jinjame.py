1a37a5ee84efe77cf49d06390395cf46361d5349
Joshua McKenty <jmckenty@pivotal.io>
2015-11-25 20:50:24 -0800

    Matching current schema for components and standards.

--- jinjame.py
+++ jinjame.py
@@ -3,9 +3,11 @@
 import click
 import os
 import yaml
+import pyaml
 import dpath
 import glob
 from jinja2 import Environment, FileSystemLoader
+import datetime
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_ENVIRONMENT = Environment(
@@ -13,7 +15,9 @@ TEMPLATE_ENVIRONMENT = Environment(
     loader=FileSystemLoader(os.path.join(PATH, 'templates')),
     trim_blocks=True,
     lstrip_blocks=True)
+
 TEMPLATE_ENVIRONMENT.filters['yaml_dump'] = yaml.dump
+TEMPLATE_ENVIRONMENT.filters['timestamp'] = datetime.datetime.now
 
 
 def render_template(template_filename, context):
@@ -24,13 +28,34 @@ def render_template(template_filename, context):
 @click.argument('inputs', nargs=-1, type=click.Path(exists=True))
 @click.argument('output', nargs=1, type=click.File('w'))
 def create_ssp_yaml(inputs, output):
-    context = {}
+    context = {'inputs' : []}
     for path in inputs:
         for filename in glob.glob("%s/*.y*ml" % (path)):
-            dpath.util.merge(context, yaml.load(open(filename, 'r')))
-    def dpath_query(expr):
-        return dpath.util.search(context, expr)
-    context.update({'dpath_query' : dpath_query})
+            print "loading %s" % (filename)
+            obj_tree = yaml.load(open(filename, 'r'))
+            context['inputs'].append(filename)
+            if 'system' not in obj_tree: # old schema
+                dpath.util.merge(context, { filename: obj_tree })
+            else:
+                dpath.util.merge(context,
+                    { obj_tree['system']: { obj_tree['name']: obj_tree } })
+    print pyaml.dump(context)
+    def dpath_loop(expr):
+        print "Looping thru dpath of %s" % (expr)
+        return dpath.util.search(context, expr, yielded=True)
+    def dpath_get(expr):
+        print "Getting %s" % (expr)
+        return dpath.util.get(context, expr)
+    def dpath_search(expr):
+        print "Searching for %s" % (expr)
+        try:
+            return dpath.util.search(context, expr)
+        except:
+            return ""
+    context.update({'dpath_search' : dpath_search,
+                    'dpath_loop' : dpath_loop,
+                    'dpath_get' : dpath_get
+                    })
     yml = render_template('pws-system.template', context)
     output.write(yml)
 
